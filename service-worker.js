826051c4d57feb3b69bed9c565a3ecf239dd7b04
Ethan <pieperejp@gmail.com>
2023-03-27 03:59:09 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
-var APP_PREFIX = 'ApplicationName_';     // Identifier for this app (this needs to be consistent across every cache update)
+var APP_PREFIX = 'SecurityApp_';     // Identifier for this app (this needs to be consistent across every cache update)
 var VERSION = 'version_01';              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION;
 var URLS = [                            // Add URL you want to cache in this list.
   '/SecuirtyApp/',                     // If you have separate JS/CSS files,
   '/SecurityApp/index.html',            // add path to those files here
+  '/SecurityApp/app.js',
   '/SecurityApp/manifest.json',
   '/SecurityApp/style.css',
   '/SecurityApp/script.js'

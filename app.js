20309b9f37dcdbcdd2320014c2a259b1cb068898
Ethan <pieperejp@gmail.com>
2023-03-27 05:28:50 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // Registering Service Worker
+/*
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/SecurityApp/service-worker.js', { scope: '/SecurityApp/' });
 }
-
-/*
+*/
 // Delay serviceWorker register
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/SecurityApp/service-worker.js', { scope: '/SecurityApp/' });
     });
 }
-*/

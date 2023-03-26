882cfa8ee4233d2eed7c250d35e04c5839bf582e
Ethan <pieperejp@gmail.com>
2023-03-27 05:44:46 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,23 @@ if (navigator.serviceWorker) {
 // Delay serviceWorker register
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/SecurityApp/service-worker.js', { scope: '/SecurityApp/' });
+        navigator.serviceWorker.register('/SecurityApp/service-worker.js', { scope: '/SecurityApp/' })
+            .then(function (registration) {
+                const subscribeOptions = {
+                    userVisibleOnly: true,
+                    applicationServerKey: urlBase64ToUint8Array(
+                        'BOhTPMafuHF2MnhTet7TGua1ZBTIQ2BckK4fazZqJpdEfWvaS7tSaRei_f67PJdbURTmHHtvso1CstosLi18SZs',
+                    ),
+                };
+
+                return registration.pushManager.subscribe(subscribeOptions);
+            })
+            .then(function (pushSubscription) {
+                console.log(
+                    'Received PushSubscription: ',
+                    JSON.stringify(pushSubscription),
+                );
+                return pushSubscription;
+            });
     });
 }
